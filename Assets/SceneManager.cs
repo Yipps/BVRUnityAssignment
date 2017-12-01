072b39dbe96db0d76620603c7d866951d358ffd0
Lawrence-WorkStation <lawrence.yip@stonybrook.edu>
2017-12-01 00:14:08 -0500

    Created a few cell paths and duplicated cell prefabs with added movement scripts

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DSAM.Utility;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,6 @@ public class SceneManager : MonoBehaviour {
     public GameObject Leukocyte;
     public GameObject Thrombocyte;
 
-    
     public float erythrocyteRate;
     public float leukocyteRate;
     public float ThrombocyteRate;
@@ -17,16 +17,20 @@ public class SceneManager : MonoBehaviour {
     public float leukocyteRadius;
     public float thrombocyteRadius;
 
-
-    
     public bool isSpawningErythrocyte;
     public bool isSpawningLeukocyte;
     public bool isSpawningThrombocyte;
+
+    public Utility_Path[] cellPaths;
     
 
 
     // Use this for initialization
     void Start () {
+        erythrocyteRate = 1f;
+        leukocyteRate = 1f;
+        ThrombocyteRate = 1f;
+
 
         if(isSpawningErythrocyte)
             InvokeRepeating("SpawnErythrocyte", 1f, erythrocyteRate);
@@ -37,31 +41,29 @@ public class SceneManager : MonoBehaviour {
         if (isSpawningThrombocyte)
             InvokeRepeating("SpawnThrombocyte", 1f, ThrombocyteRate);
 
-
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-        
-	}
 
     void SpawnErythrocyte()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-erythrocyteRadius, erythrocyteRadius), Random.Range(-erythrocyteRadius, erythrocyteRadius), 0);
-        Instantiate(Erythrocyte, spawnPosition, Quaternion.identity);
+        Vector3 pathPos = cellPaths[Random.Range(-cellPaths.Length, cellPaths.Length)].transform.position;
+        GameObject instance = Instantiate(Erythrocyte, pathPos, Quaternion.identity);
+        //Utility_FollowSpline cellMovement = instance.AddComponent<Utility_FollowSpline>();
+
+
     }
 
     void SpawnLeukocyte()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-leukocyteRadius, leukocyteRadius), Random.Range(-leukocyteRadius, leukocyteRadius), 0);
-        Instantiate(Leukocyte, spawnPosition, Quaternion.identity);
+        Vector3 pathPos = cellPaths[Random.Range(-cellPaths.Length, cellPaths.Length)].transform.position;
+        GameObject instance = Instantiate(Leukocyte, pathPos, Quaternion.identity);
+        //Utility_FollowSpline cellMovement = instance.AddComponent<Utility_FollowSpline>();
     }
+
     void SpawnThrombocyte()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-thrombocyteRadius, thrombocyteRadius), Random.Range(-thrombocyteRadius, thrombocyteRadius), 0);
-        Instantiate(Thrombocyte, spawnPosition, Quaternion.identity);
+        Vector3 pathPos = cellPaths[Random.Range(-cellPaths.Length, cellPaths.Length)].transform.position;
+        GameObject instance = Instantiate(Thrombocyte, pathPos, Quaternion.identity);
+        //Utility_FollowSpline cellMovement = instance.AddComponent<Utility_FollowSpline>();
     }
 
 }

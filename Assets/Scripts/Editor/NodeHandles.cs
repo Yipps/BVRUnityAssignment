32a53053b73b51e2e1a0d9ab073865879e37cfb0
Lawrence-WorkStation <lawrence.yip@stonybrook.edu>
2017-12-03 16:37:55 -0500

    Created basic editor script to create handles for path nodes

---
+++ Assets/Scripts/Editor/NodeHandles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DSAM.Utility;
+
+
+[CustomEditor(typeof(Utility_Path))]
+public class NodeHandles : Editor {
+
+    Utility_Path path;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    void OnSceneGUI()
+    {
+        
+        
+        
+        path = (Utility_Path)target;
+        for (int i = 0; i < path.nodes.Count; i++)
+        {
+            Handles.color = Color.black;
+            path.nodes[i] = Handles.PositionHandle(path.nodes[i], Quaternion.identity);
+        }
+        
+    }
+}
